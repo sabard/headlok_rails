c3516c5accb2a3339c97dd27a2aa272cbe5bca4a
Sabar Dasgupta <sabar@orchive.com>
2014-02-16 08:21:03 -0500

    sendgrid pls

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ HeadlokRails::Application.routes.draw do
   
   root 'statics#index'
 
-  get 'notifier/security_notice/:email' => 'notifications#security_notice'
+  match 'notifier/security_notice/:email' => 'notifications#security_notice', via: [:post] 
 end

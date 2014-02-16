f1b34ad24ea107a4da8747d2adda0acc9de1e717
Sabar Dasgupta <sabar@orchive.com>
2014-02-16 08:43:01 -0500

    post pls

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ HeadlokRails::Application.routes.draw do
   
   root 'statics#index'
 
-  match 'notifier/security_notice/:email' => 'notifications#security_notice', via: [:post] 
+  match 'notifier/security_notice/' => 'notifications#security_notice', via: [:post] 
 end
